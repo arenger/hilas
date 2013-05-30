8e3f9f5c882a42dd623e9e01afc7d9ef316ad4cc
Alex Renger <arenger@gmail.com>
2013-05-30 17:21:36 -0600

    init

---
+++ src/main/java/edu/uccs/arenger/hilas/Hilas.java
@@ -0,0 +1,13 @@
+package edu.uccs.arenger.hilas;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hilas 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
